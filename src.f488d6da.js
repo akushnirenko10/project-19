012d44a216ae7048c2f3c658a8667253718ae878
akushnirenko10 <97456217+akushnirenko10@users.noreply.github.com>
2022-05-12 14:37:27 +0000

    Deploying to gh-pages from @ akushnirenko10/project-19@16450617073f15937f975f741a8b7c0e0d51a5ba 🚀

--- src.f488d6da.js
+++ src.f488d6da.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-19/src.ef89e8e9.js.map
+//# sourceMappingURL=/project-19/src.f488d6da.js.map
