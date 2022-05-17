ea28f9e456c2dd287bfc4c8300bff7eebd062d41
LazyFatRaccoon <vladimir.todorov.ukraine@gmail.com>
2022-05-17 21:02:18 +0300

    модалка + правки

--- src/MenuBtn.js
+++ src/MenuBtn.js
@@ -5,15 +5,15 @@
       header_menu: document.querySelector('[header_data-menu]'),
      };
   
-    refs.header_openMenuBtn.addEventListener('click', toggleModal);
-    refs.header_closeMenuBtn.addEventListener('click', toggleModal);
+    refs.header_openMenuBtn.addEventListener('click', toggleMenu);
+    refs.header_closeMenuBtn.addEventListener('click', toggleMenu);
 
     var links = document.getElementsByClassName('pop-up-menu__navigation__element');
         for (var i = 0; i < links.length; i++) {
-            links[i].addEventListener('click', toggleModal);
+            links[i].addEventListener('click', toggleMenu);
     }
   
-    function toggleModal() {
+    function toggleMenu() {
         console.log(1);
       refs.header_menu.classList.toggle("is-open");
       //refs.openMenuBtn.classList.toggle('is-open');
